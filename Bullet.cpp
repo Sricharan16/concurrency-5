52736feeb989b0936d9ebb1324221582115e8e9a
Sricharan16 <cs16btech11044@iith.ac.in>
2018-03-05 16:02:17 +0530

    corrected the error in bullet.cpp

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,45 +1,57 @@
-#include"Button.h"
-#include<QGraphicsTextItem>
-#include<QBrush>
-#include<QGraphicsSceneHoverEvent>
-#include<QGraphicsSceneMouseEvent>
-Button::Button(QString name, QGraphicsItem *parent):QGraphicsRectItem(parent)
-{
-    //draw the rect
-    setRect(0,0,200,50);
-    QBrush brush;
-    brush.setStyle(Qt::SolidPattern);
-    brush.setColor(Qt::darkCyan);
-    setBrush(brush);
-    text  = new QGraphicsTextItem(name,this);
-    int xPos =rect().width()/2- text->boundingRect().width()/2;
-    int yPos =rect().height()/2- text->boundingRect().height()/2;
-    text->setPos(xPos,yPos);
-    //allow responding to hover events
-    setAcceptHoverEvents(true);
+#include "Bullet.h"
+#include <QTimer>
+#include <QGraphicsScene>
+#include <QList>
+#include "enemy.h"
+#include<typeinfo>
+#include <QDebug>
+#include<QKeyEvent>
+#include <QPixmap>
+#include<qmath.h>
+#include "Game.h"
+#include<QGraphicsPixmapItem>
+extern Game * game;
+Bullet::Bullet(): QObject(), QGraphicsPixmapItem(){
+    // drew the rect
+    setPixmap(QPixmap(":/images/bullet.png"));
+    setScale(0.15);
 
+    // connect
+    QTimer * timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(move()));
 
+    timer->start(50);
 }
 
-void Button::mousePressEvent(QGraphicsSceneMouseEvent *event)
-{
-    emit clicked();
-}
+void Bullet::move(){
+    // if bullet collides with enemy, destroy both
+    QList<QGraphicsItem *> colliding_items = collidingItems();
+    for (int i = 0, n = colliding_items.size(); i < n; ++i){
+        if (typeid(*(colliding_items[i])) == typeid(Enemy)){
+            //increase the score also here
+            game->score->increase();
+            // remove them both
+            scene()->removeItem(colliding_items[i]);
+            scene()->removeItem(this);
+            // delete them both
+            delete colliding_items[i];
+            delete this;
+            return;
+        }
+    }
 
-void Button::hoverEnterEvent(QGraphicsSceneHoverEvent *event)
-{
-    //changing color
-    QBrush brush;
-    brush.setStyle(Qt::SolidPattern);
-    brush.setColor(Qt::cyan);
-    setBrush(brush);
-}
+    // move bullet up
+    int STEP_SIZE = 30;
+        double theta = rotation(); // degrees
 
-void Button::hoverLeaveEvent(QGraphicsSceneHoverEvent *event)
-{
-    QBrush brush;
-    brush.setStyle(Qt::SolidPattern);
-    brush.setColor(Qt::darkCyan);
-    setBrush(brush);
+        double dy = STEP_SIZE * qSin(qDegreesToRadians(theta));
+        double dx = STEP_SIZE * qCos(qDegreesToRadians(theta));
+
+        setPos(x()+dx, y()+dy);
+    if (pos().y() < 0 || pos().x()<0 || pos().y() + 50>600 ){
+        scene()->removeItem(this);
+        //qDebug()<<"Removed";
+        delete this;
+    }
 }
 
